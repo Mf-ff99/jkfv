916896845e80c156e2a99e9ff8c9647a9fde96c4
Mark Force <force.j.mark@gmail.com>
2021-01-15 13:34:23 -0500

    added address and office number to contact page, changed colors on contact page

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -7,58 +7,77 @@ import './contact.css'
 export default function Contact() {
     return (
         <>
-        <Animated animationIn="fadeIn" animationOut="fadeOut">
+            <Animated animationIn="fadeIn" animationOut="fadeOut">
 
-            <section className="contact-page">
-                <div className="about-container">
-                    <div className="contact-upper">
+                <section className="contact-page">
+                    <div className="about-container">
+                        <div className="contact-upper">
 
-                        <h2>Contact Us</h2>
-                        <span>352-427-2560</span>
-                        <span><a className="email" href="mailto:jordy@jordy.com" >jordy@jordy.com</a></span>
-                        <hr />
-                        <h6>Call or email us, or simply fill out the form below and we will be in touch with you as soon as possible.</h6>
-                        <HashLink smooth to='#form'>
-                            <Animated animationIn='slideInUp' animationInDuration={1200}>
-                            <img className='work-scroll-arrow contact-arrow' src={Images.DownArrow} alt='scroll-down arrow' />
-                            </Animated>
+                            <h2>Contact Us</h2>
+                            <div className='contact-info-container'>
+    <div className='contact-info-line-address'>
+
+                            <h3>Address</h3>
+                            <span>
+                                10197 SE 144th PL, Summerfield,FL 34491
+                        </span>
+    </div>
+    <div className='contact-info-line'>
+                        <h3>Office #</h3>
+                            <span>352-288-3999</span>
+                            </div>
+                            <div className='contact-info-line'>
+                            <h3>Cell #</h3>
+                            <span>352-427-2560</span>
+                            </div>
+                            <div className='contact-info-line'>
+                            
+                            <span><a className="email" href="mailto:jordy@jordy.com" >jordy@jordy.com</a></span>
+                            </div>
+                            </div>
+                            <hr />
+                            <h6>Call or email us, or simply fill out the form below and we will be in touch with you as soon as possible.</h6>
+                            <HashLink smooth to='#form'>
+                                <Animated animationIn='slideInUp' animationInDuration={1200}>
+                                    <img className='work-scroll-arrow contact-arrow' src={Images.DownArrow} alt='scroll-down arrow' />
+                                </Animated>
                             </HashLink>
 
-                    </div>
-                
-                    <hr />
-                    <div className='contact-container-lower'>
+                        </div>
 
-                    <div className='contact-lower'>
-                        
-                            <form id='form' className="form-style-4" action="" method="post">
-                                <label htmlFor="field1">
-                                    <span></span><input placeholder='Your Name' type="text" name="field1" required/>
-                                </label>
-                                <label htmlFor="field2">
-                                    <span></span><input placeholder='youremail@email.com' type="email" name="field2" required />
-                                </label>
-                                <label htmlFor="field3">
-                                    <span></span><input placeholder='Subject' type="text" name="field3" required />
-                                </label>
-                                <label htmlFor="field5">
-                                    <span></span><input  placeholder='Company'type="text" name="field5" required />
-                                </label>
-                                <label htmlFor="field6">
-                                    <span></span><textarea name="field6"  placeholder='Your message here' required></textarea>
-                                </label>
-                                <label>
-                                    <span> </span><input type="submit" value="Submit" />
-                                </label>
-                            </form>
-                    
-                    </div>
-                    </div>
+                        <hr />
+                        <div className='contact-container-lower'>
 
-                </div>
-            </section>
+                            <div className='contact-lower'>
 
-            {/* <section className="contact-page">
+                                <form id='form' className="form-style-4" action="" method="post">
+                                    <label htmlFor="field1">
+                                        <span></span><input placeholder='Your Name' type="text" name="field1" required />
+                                    </label>
+                                    <label htmlFor="field2">
+                                        <span></span><input placeholder='youremail@email.com' type="email" name="field2" required />
+                                    </label>
+                                    <label htmlFor="field3">
+                                        <span></span><input placeholder='Subject' type="text" name="field3" required />
+                                    </label>
+                                    <label htmlFor="field5">
+                                        <span></span><input placeholder='Company' type="text" name="field5" required />
+                                    </label>
+                                    <label htmlFor="field6">
+                                        <span></span><textarea name="field6" placeholder='Your message here' required></textarea>
+                                    </label>
+                                    <label>
+                                        <span> </span><input type="submit" value="Submit" />
+                                    </label>
+                                </form>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </section>
+
+                {/* <section className="contact-page">
                 <div className="about-container-lower">
                     <div className='contact-lower'>
                     <p>Contact Us</p>
@@ -68,7 +87,7 @@ export default function Contact() {
                     
                     <input type="text" placeholder="yourname here!"></input>
                     {/* <br /> */}
-            {/* <label htmlFor="company-name">Associated Company</label>
+                {/* <label htmlFor="company-name">Associated Company</label>
                             <input type="text" name="company-name" id="company-name"></input>
                             <label htmlFor="phone">Enter your phone number</label>
                             
@@ -80,7 +99,7 @@ export default function Contact() {
                         </div> */}
 
 
-                        </Animated>
+            </Animated>
         </>
     )
 }
