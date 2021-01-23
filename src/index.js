6da12f5a25a06918b0a455730e9fd2a4b17a5f4b
Mark Force <force.j.mark@gmail.com>
2021-01-23 17:41:13 -0500

    working on missing boostrap cdn bug

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom'
 import './index.css';
 import App from './App'
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
+
+
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
