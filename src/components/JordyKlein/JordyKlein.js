a30a3317f1436621fcffcceebdcf5b14a5f398ba
Mark Force <force.j.mark@gmail.com>
2021-01-24 11:51:39 -0500

    added justify content center to contact info lines

--- src/components/JordyKlein/JordyKlein.js
+++ src/components/JordyKlein/JordyKlein.js
@@ -24,7 +24,7 @@ const JordyKlein = () => {
             <Animated animationInDuration={1000} animationIn='slideInUp'>
 
             <p>
-            Jordan "Jordy" Klein Jr. is a legendary aerial and aquatic cinematographer, and owner of Jordan Klein Film & Video and Xcam Aerials Inc. Jordy has worked for years alongside his Academy Award-Winning D/P and father, Jordan Klein Sr. Jordy has shot numerous movies, commercials, documentaries, and industrial films for clients such as CSI Miami, One Tree Hill, HBO, Netflix, Sea World, Sea-Doo Watercraft, Discovery Channel, and most Hollywood studios. Jordy is an accomplished R/C, and drone pilot and holds a part 107 drone license. Jordy is also a commercial full-sized helicopter and fixed-wing pilot. Additionally, Jordy is an accomplished Steadicam operator and Underwater cinematographer and holds an advanced mixed-gas scuba license.
+            Jordan "Jordy" Klein Jr. is a legendary aerial and aquatic cinematographer, and owner of Jordan Klein Film & Video and XCam Aerials Inc. He has worked for years alongside his Academy Award-Winning D/P and father, Jordan Klein Sr. Jordy has shot numerous movies, commercials, documentaries, and industrial films for clients such as CSI Miami, One Tree Hill, HBO, Netflix, Sea World, Sea-Doo Watercraft, Discovery Channel, and most Hollywood studios. He is an commercial full-sized helicopter and fixed-wing pilot, an accomplished R/C and drone pilot, and holds a part 107 drone license. Additionally, Jordy is a master Steadicam operator, Underwater cinematographer, and holds his advanced mixed-gas scuba license.
             </p>
             </Animated>
             </div>
